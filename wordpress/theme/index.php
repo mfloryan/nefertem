7b99ddf47dbf25009afbf8350d1a60e654f43166
Marcin Floryan <marcin.floryan@gmail.com>
2011-10-14 14:11:36 +0100

    Some meta info

--- wordpress/theme/index.php
+++ wordpress/theme/index.php
@@ -1,20 +1,31 @@
 <?php
-/*
-    nefertem - A WordPress template
-
-    Copyright (C) 2011 Marcin Floryan, mmsquare limited, http://www.mmsquare.com/
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    For the full license see the LICENSE file.
+/**
+ *  nefertem - A WordPress template
+ *  
+ *  Copyright (C) 2011 Marcin Floryan, mmsquare limited, http://www.mmsquare.com/
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *   For the full license see the LICENSE file.
+ *   
+ * @package WordPress
+ * @subpackage Toolbox
+ * @since Toolbox 0.1
  */
 
+	get_header(); 
+ 
 ?>
+
+
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
