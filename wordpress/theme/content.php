489d7351ed0394e94fd0654ee2ea2e2641987e71
Marcin Floryan <marcin.floryan@gmail.com>
2011-10-27 22:38:19 +0100

    Fixed the date and tags are now shown nicely

--- wordpress/theme/content.php
+++ wordpress/theme/content.php
@@ -3,7 +3,7 @@
 
     <div class="header"><h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
     </div>
-    <div class="info"> <time datetime="<?php the_date('Y-m-d') ?>"><?php the_date('D j F Y') ?></time> | <?php the_tags() ?> </div>
+    <div class="info"> <time datetime="<?php echo get_the_date('Y-m-d'); ?>"><?php the_date('D j F Y'); ?></time> <?php if (get_the_tags()):?> | <?php the_tags(); ?> <?php endif; ?></div>
 
     <?php the_content(); ?>
 
