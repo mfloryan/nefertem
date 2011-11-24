82cf467cf53e189b3d84c6b33d7c333a3593c94f
Marcin Floryan <marcin.floryan@gmail.com>
2011-11-24 15:50:49 +0000

    Comments

--- wordpress/theme/comments.php
+++ wordpress/theme/comments.php
@@ -28,12 +28,7 @@
 ?>
 
 <?php if ( have_comments() ) : ?>
-    <h3 id="comments-title">
-        <?php
-            printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'toolbox' ),
-                number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-        ?>
-    </h3>
+    <h2>Comments<span class="alignright"><?php echo get_comments_number(); ?></span></h2>
 
     <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
     <nav id="comment-nav-above">
--- wordpress/theme/functions.php
+++ wordpress/theme/functions.php
@@ -136,35 +136,37 @@ function nefertem_comment( $comment, $args, $depth ) {
     } else {
 	?>
 	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
-		<article id="comment-<?php comment_ID(); ?>" class="comment">
-			<footer>
+		<div id="comment-<?php comment_ID(); ?>" class="comment">
+			<div>
+                <?php echo get_avatar( $comment, 50 ); ?>
+
+                <div class="comment-meta">
+                    <a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time pubdate datetime="<?php comment_time( 'c' ); ?>">
+                    <?php
+                        /* translators: 1: date, 2: time */
+                        printf( __( '%1$s at %2$s', 'toolbox' ), get_comment_date(), get_comment_time() ); ?>
+                    </time></a>
+                    <?php edit_comment_link( __( '(Edit)', 'toolbox' ), ' ' );
+                    ?>
+                </div><!-- .comment-meta .commentmetadata -->
+
 				<div class="comment-author vcard">
-					<?php echo get_avatar( $comment, 40 ); ?>
+
 					<?php printf( __( '%s <span class="says">says:</span>', 'toolbox' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
 				</div><!-- .comment-author .vcard -->
-				<?php if ( $comment->comment_approved == '0' ) : ?>
+
+                <?php if ( $comment->comment_approved == '0' ) : ?>
 					<em><?php _e( 'Your comment is awaiting moderation.', 'toolbox' ); ?></em>
 					<br />
 				<?php endif; ?>
-
-				<div class="comment-meta commentmetadata">
-					<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time pubdate datetime="<?php comment_time( 'c' ); ?>">
-					<?php
-						/* translators: 1: date, 2: time */
-						printf( __( '%1$s at %2$s', 'toolbox' ), get_comment_date(), get_comment_time() ); ?>
-					</time></a>
-					<?php edit_comment_link( __( '(Edit)', 'toolbox' ), ' ' );
-					?>
-				</div><!-- .comment-meta .commentmetadata -->
-			</footer>
+			</div>
 
 			<div class="comment-content"><?php comment_text(); ?></div>
 
 			<div class="reply">
 				<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
 			</div><!-- .reply -->
-		</article><!-- #comment-## -->
-
+		</div><!-- #comment-## -->
 	<?php
     }
 }
