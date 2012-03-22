8092d2938fc966f5d9f17ae152a7409feb56a362
Marcin Floryan <marcin.floryan@gmail.com>
2012-03-22 09:33:23 +0000

    Front page content is now an article.

--- wordpress/theme/front-page.php
+++ wordpress/theme/front-page.php
@@ -82,7 +82,9 @@
     <div class="grid_8 alpha omega">
 
         <?php while (have_posts()) : the_post(); ?>
-            <?php the_content(); ?>
+            <article>
+	        <?php the_content(); ?>
+	    </article>
         <?php endwhile; // end of the loop. ?>
 
     </div>
